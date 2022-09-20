98a0b4c82ba7de21e77c8c9ca7ff486a6bda49c0
bibinkunjumon <bibinkunjumon2020@gmail.com>
2022-09-20 20:39:00 +0300

    initial commit

---
+++ todoapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Todos(models.Model):
+    task_name = models.CharField(max_length=120)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    status = models.BooleanField(default=False)
+    created_date = models.DateField(auto_created=True)
+
+    def __str__(self):
+        return self.task_name
---
+++ todoapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+#registration
+#login
+#add
+#list
+#update
+#delete
+
