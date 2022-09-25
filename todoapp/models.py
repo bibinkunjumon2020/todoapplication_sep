07358d08fc06505483ab9f7e61aa0daf7d6663b3
bibinkunjumon <bibinkunjumon2020@gmail.com>
2022-09-25 09:41:32 +0300

    Its 9th see finish

--- todoapp/models.py
+++ todoapp/models.py
@@ -6,7 +6,7 @@ class Todos(models.Model):
     task_name = models.CharField(max_length=120)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     status = models.BooleanField(default=False)
-    created_date = models.DateField(auto_created=True)
+    created_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.task_name
